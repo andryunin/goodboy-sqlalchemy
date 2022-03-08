c177be4c9bf6810d480c3d4b4f63eca5155f26fd
Maxim Andryunin <maxim.andryunin@gmail.com>
2022-03-08 13:02:47 +0700

    feat: add SQLAlchemy column type to schema mapping

---
+++ tests/test_schemas.py
@@ -0,0 +1,59 @@
+from unittest.mock import Mock
+
+import goodboy as gb
+import pytest
+import sqlalchemy as sa
+
+from goodboy_sqlalchemy.schemas import (
+    SchemaBuilder,
+    SchemaBuilderError,
+    SimpleColumnSchemaFactory,
+    StringColumnSchemaFactory,
+)
+
+
+@pytest.mark.parametrize("nullable", [True, False])
+def test_simple_column_schema_factory(nullable):
+    column = sa.Column("dummy", sa.Integer, nullable=nullable)
+
+    schema = object()
+    schema_class = Mock(return_value=schema)
+
+    factory = SimpleColumnSchemaFactory(schema_class)
+    result = factory.build(column)
+
+    assert result is schema
+    schema_class.assert_called_once_with(allow_none=nullable)
+
+
+@pytest.mark.parametrize("nullable", [True, False])
+def test_string_column_schema_factory(nullable):
+    column = sa.Column("dummy", sa.String(length=255), nullable=nullable)
+
+    factory = StringColumnSchemaFactory()
+    result = factory.build(column)
+
+    assert result == gb.Str(allow_none=nullable, max_length=255)
+
+
+def test_schema_builder_when_type_mapped():
+    column = sa.Column("dummy", sa.Integer, nullable=False)
+
+    schema = object()
+    schema_factory = Mock()
+    schema_factory.build = Mock(return_value=schema)
+
+    builder = SchemaBuilder({
+        sa.Integer: schema_factory
+    })
+
+    assert builder.build(column) is schema
+    schema_factory.build.assert_called_once_with(column)
+
+
+def test_schema_builder_when_type_unmapped():
+    column = sa.Column("dummy", sa.Integer, nullable=False)
+    builder = SchemaBuilder({})
+
+    with pytest.raises(SchemaBuilderError):
+        assert builder.build(column)
