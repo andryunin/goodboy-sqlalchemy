6ba42a22f672ee153395d509bdfef8b3dfa6390a
Maxim Andryunin <maxim.andryunin@gmail.com>
2024-05-28 21:02:55 +0700

    chore: bump version to 0.2.4

--- src/goodboy_sqlalchemy/__init__.py
+++ src/goodboy_sqlalchemy/__init__.py
@@ -12,7 +12,7 @@ from goodboy_sqlalchemy.mapped import (
 )
 from goodboy_sqlalchemy.messages import DEFAULT_MESSAGES
 
-__version__ = "0.2.3"
+__version__ = "0.2.4"
 
 __all__ = [
     "column_schema_builder",
